0d22b96961cc71c104b981a3115098bd51acd435
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2018-12-17 23:30:19 +0800

    ...

--- LCS_Algorithm/LCS_Algorithm.cpp
+++ LCS_Algorithm/LCS_Algorithm.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
-
-
+//Longest Common Subsequence Problem
+#include <iostream>
+#include <string>
+using namespace std;
+typedef struct Sequence {
+	string str1,str2;
+	int size1, size2;
+}seq;
+void LCS(seq seq1);
 int main()
 {
+	seq seq1;
+	cin >> seq1.str1>>seq1.str2;
+	seq1.size1 = seq1.str1.size();
+	seq1.size2 = seq1.str2.size();
     return 0;
+}
+void LCS(seq seq1) {
+
+
 }
 
