b277c795fa2e07d63b5ce3a9303cd54565bead0a
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2018-12-18 13:11:02 +0800

    Update LCS_Algorithm.cpp

--- LCS_Algorithm/LCS_Algorithm.cpp
+++ LCS_Algorithm/LCS_Algorithm.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 //Longest Common Subsequence Problem
+//Longest Common Subsequence Problem
 #include <iostream>
 #include <string>
 using namespace std;
@@ -20,7 +21,27 @@ int main()
     return 0;
 }
 void LCS(seq seq1) {
-
-
+	int i,j;
+	int size1=seq.size1;
+	int size2=seq.size2;
+	string str1=seq.str1 , str2=seq.str2;
+	int **sub=new int*[size1];
+	int **length=new int*[size1];
+	for(i=0;i!=size1;i++){
+		*sub[i]=new int[size2];
+		*length=new int[size2];
+		for(j=0;j!=size2;j++)
+			sub[i][j]=0;
+			length[i][j]=0;
+	}  
+	for(i=0;i!=size1;i++){
+		for(j=0;j!=size2;j++)
+			if(str[i]==str[j]){
+				sub[i][j]=0//右上
+				if(i-1<0||j-1<0)	length[i][j]=0;
+				else length[i][j]=length[i-1][j-1]+1;
+			}else{
+				
+			} 
+	}
 }
-
