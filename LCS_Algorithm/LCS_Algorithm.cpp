432840c24b8cdd107a46a4e40e67a158c8f65f04
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2018-12-19 13:03:36 +0800

    Update LCS_Algorithm.cpp

--- LCS_Algorithm/LCS_Algorithm.cpp
+++ LCS_Algorithm/LCS_Algorithm.cpp
@@ -3,7 +3,6 @@
 
 #include "stdafx.h"
 //Longest Common Subsequence Problem
-//Longest Common Subsequence Problem
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,36 +11,48 @@ typedef struct Sequence {
 	int size1, size2;
 }seq;
 void LCS(seq seq1);
-int main()
-{
+int main(){
 	seq seq1;
 	cin >> seq1.str1>>seq1.str2;
 	seq1.size1 = seq1.str1.size();
 	seq1.size2 = seq1.str2.size();
+	LCS(seq1);
     return 0;
 }
 void LCS(seq seq1) {
 	int i,j;
-	int size1=seq.size1;
-	int size2=seq.size2;
-	string str1=seq.str1 , str2=seq.str2;
+	int size1=seq1.size1;
+	int size2=seq1.size2;
+	string str1=seq1.str1 , str2=seq1.str2;
 	int **sub=new int*[size1];
 	int **length=new int*[size1];
 	for(i=0;i!=size1;i++){
-		*sub[i]=new int[size2];
-		*length=new int[size2];
+		sub[i]=new int[size2];
+		length[i]=new int[size2];
 		for(j=0;j!=size2;j++)
 			sub[i][j]=0;
 			length[i][j]=0;
 	}  
 	for(i=0;i!=size1;i++){
 		for(j=0;j!=size2;j++)
-			if(str[i]==str[j]){
-				sub[i][j]=0//右上
-				if(i-1<0||j-1<0)	length[i][j]=0;
-				else length[i][j]=length[i-1][j-1]+1;
+			if(str1[i]==str2[j]){
+				sub[i][j]=0;//右上
+				if(i-1<0||j-1<0)
+					length[i][j]=1;
+				else 
+					length[i][j]=length[i-1][j-1]+1;
 			}else{
-				
+				if(i-1<0||j-1<0)	
+					length[i][j]=1;
+				if(length[i-1][j]>=length[i][j-1]){
+					sub[i][j]=1;//上
+					length[i][j]=length[i-1][j]; 
+				}else{
+					sub[i][j]=2;//左
+					length[i][j]=length[i][j-1];
+				}
 			} 
 	}
+	//print LCS
+	cout<<length[size1][size2];
 }
